b5ad7d2096b161c2363bdd3df537435e1e003f98
mtlmacedo <42045383+mtlmacedo@users.noreply.github.com>
2021-04-29 21:26:15 -0300

    Code Cleaning

--- src/inf011/service/PluginService.java
+++ src/inf011/service/PluginService.java
@@ -1,10 +1,5 @@
 package inf011.service;
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -59,40 +54,10 @@ public class PluginService {
 		    if(documentName == null || documentName.isEmpty()) {  		    
 				throw new Exception("Não existe plugin que suporte este arquivo");
 		    }else {    		    	
-		    	
-		    	//URL u = (new File(this.pluginPath + documentName + ".jar")).toURL();
-		    	//URL[] pluginsURLs = { new URL("src.plugins") };
-		    	
-				//URLClassLoader URLLoader = (URLClassLoader)ClassLoader.getSystemClassLoader();//new URLClassLoader(pluginsURLs);
-				Class<?> docClass = Class.forName(this.pluginPath + documentName); //URLLoader.loadClass("PDFDocument");
+		    	Class<?> docClass = Class.forName(this.pluginPath + documentName);
 				IDocument doc = (IDocument) docClass.newInstance();
-				//URLLoader.close();
 				return doc;
-				//URLClassLoader classLoader = (URLClassLoader)ClassLoader.getSystemClassLoader();
-//				Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
-//				method.setAccessible(true);
-//				method.invoke(classLoader, url);
-//								
-//				Class sysclass = URLClassLoader.class;
-//				
-//		        Method method = sysclass.getDeclaredMethod("addURL", parameters);
-//		        method.setAccessible(true);
-//		        method.invoke(sysloader, new Object[]{u});
-//		        sysloader.loadClass(u.getClass());
-//		        Class.forName("myplugin.MyPlugin").newInstance();
-//				
-//				
-//				
-//		    	Class<?> documentClass = Class.forName(this.pluginPath + documentName);
-//		    	
-//		    	List<Method> methods = Arrays.asList(documentClass.getDeclaredMethods());
-//		    	Method getInstanceMethod = methods.stream()
-//		    			  .filter(method -> method.getName().equals("getInstance"))
-//		    			  .findAny()
-//		    			  .orElse(null);	    	
-//				 
-//		    	IDocument document =  (IDocument) getInstanceMethod.invoke(null);
-//		    	return document;
+				
 		    }
 		}catch (Exception e) {
 			e.printStackTrace();
